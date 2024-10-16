80b8c8c7a39bf8c44364b424ada97efa22118585
supercandela <supercandela@gmail.com>
2024-10-16 00:04:16 -0300

    agrego graficos y estadisticas

--- src/app/estadisticas/estadisticas/estadisticas.page.ts
+++ src/app/estadisticas/estadisticas/estadisticas.page.ts
@@ -1,14 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { collection, collectionData, Firestore, orderBy, query, where } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import {
-  collection,
-  collectionData,
-  Firestore,
-  orderBy,
-  query,
-  where,
-} from '@angular/fire/firestore';
 import { LegendPosition } from '@swimlane/ngx-charts';
 import { Observable } from 'rxjs';
 
@@ -22,24 +15,19 @@ export class EstadisticasPage implements OnInit {
   position: LegendPosition = LegendPosition.Below;
   fotos: any[] = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location,
-    private firestore: Firestore
-  ) {}
+  constructor(private route: ActivatedRoute, private location: Location, private firestore: Firestore) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       this.seleccion = params['seleccion'] || '';
     });
 
-    this.getAllFotos().subscribe((res) => {
-      console.log(res);
-      this.fotos = res.map((o: any) => {
-        return { name: o.usuario + o.fecha, value: o.votos, extra: o.foto };
+    this.getAllFotos().subscribe(
+      (res) => {
+        console.log(res);
+        this.fotos = res.map((o:any) => { return {name: o.usuario + o.fecha, value: o.votos, extra: o.foto} });
+        console.log(this.fotos);
       });
-      console.log(this.fotos);
-    });
   }
 
   // Método para regresar a la página anterior
